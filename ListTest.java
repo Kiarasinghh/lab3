06d5c6acc34f9a773cdf23ccbfca2a726b494771
Kiarasinghh <156370071+Kiarasinghh@users.noreply.github.com>
2024-02-01 13:51:57 -0800

    update

--- ListTest.java
+++ ListTest.java
@@ -1,20 +1,33 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class ListTest {
-    LinkedList linkedList1=new LinkedList();
+interface StringChecker { boolean checkString(String s); }
+
+
+public class ListTest implements StringChecker{
+
+    ArrayList<String> list1 = new ArrayList<>();
+    list1.add("cat");
+    list1.add("dog");
+    list1.add("strawberry");
+    list1.add("basketball");
+    list1.add("hog");
+
+    ArrayList<String> list2 = new ArrayList<>();
+    list2.add("cat");
+    list2.add("cat");
+    list2.add("cat");
+
+
+
 
     @Test 
-	public void testPrepend() {
-    linkedList1.prepend(10);
-    assertEquals(10,linkedList1.root.value);
-    assertSame(null,linkedList1.root.next);
+	public void testFilter() {
 
-    linkedList1.prepend(20);
-    assertEquals(20,linkedList1.root.value);
-    assertEquals(10,linkedList1.root.next.value);
-    assertSame(null,linkedList1.root.next.next);
+    
     }
 }
 
